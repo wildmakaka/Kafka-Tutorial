c424f15289105a19e9b94377382504bba1dbb047
david <davidd7598@gmail.com>
2022-07-12 15:27:05 +0600

    fixed json parsing

--- app/src/main/java/org/javadev/kafkaexample/config/KafkaConsumerConfig.java
+++ app/src/main/java/org/javadev/kafkaexample/config/KafkaConsumerConfig.java
@@ -11,6 +11,8 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -45,6 +47,11 @@ public class KafkaConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<String, Message> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         return factory;
+    }
+
+    @Bean
+    public RecordMessageConverter converter() {
+        return new JsonMessageConverter();
     }
 
 } // The End of Class;
